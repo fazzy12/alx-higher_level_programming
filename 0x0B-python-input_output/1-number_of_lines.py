73c519ad268a0a0405fe936848e285a94a29034a
fazzy12 <kaluifeanyi00@gmail.com>
2022-09-27 15:01:59 +0100

    input output

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -8,6 +8,6 @@ def number_of_lines(filename=""):
     """Return the number of lines in a text file."""
     lines = 0
     with open(filename) as f:
-        for line in f:
+        for lines in f:
             lines += 1
     return lines
