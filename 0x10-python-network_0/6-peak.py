2e5b0ac5a585fad9b97a8760785b424aef788d80
fazzy12 <kaluifeanyi00@gmail.com>
2022-12-21 02:19:43 +0100

    network project

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
+
 def find_peak(list_of_integers):
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
     
-"""Return a peak in a list of unsorted integers."""
 
-if list_of_integers == []:
-    return None
-
-size = len(list_of_integers)
-if size == 1:
-    return list_of_integers[0]
-
-# Divide the list into two halves
-mid = size // 2
-left = list_of_integers[:mid]
-right = list_of_integers[mid:]
-
-# Find the peak in the left and right halves
-peak_left = find_peak(left)
-peak_right = find_peak(right)
-
-# Return the larger of the two peaks
-return max(peak_left, peak_right)
 
