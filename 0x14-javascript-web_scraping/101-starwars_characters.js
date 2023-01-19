03ab9a46b3a91deaff94dad1762f8179b2c88c6b
fazzy12 <kaluifeanyi00@gmail.com>
2023-01-19 03:27:37 +0100

    print chraccters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,33 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const charactersPromises = data.characters.map(characterUrl => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            const characterData = JSON.parse(body);
+            resolve(characterData.name);
+          } else {
+            reject(error);
+          }
+        });
+      });
+    });
+    Promise.all(charactersPromises)
+      .then(characterNames => {
+        characterNames.forEach(name => {
+          console.log(name);
+        });
+      })
+      .catch(error => {
+        console.log("Error: Could not retrieve character information.");
+      });
+  } else {
+    console.log("Error: Could not retrieve movie information.");
   }
 });
-
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
-}
