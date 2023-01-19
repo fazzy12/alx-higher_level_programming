1413e396330eb2e473101a89076a24fbb42c0264
fazzy12 <kaluifeanyi00@gmail.com>
2023-01-19 01:45:06 +0100

    read file content

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+const filepath = process.argv[2];
+
+fs.readFile(filepath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
+
+// const fs = require('fs');
+// fs.readFile(process.argv[2], 'utf8', function (error, content) {
+//   console.log(error || content);
+// });
