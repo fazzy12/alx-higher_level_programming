b64e521a39f561205cd789c469bc6f24cb0cbc36
fazzy12 <kaluifeanyi00@gmail.com>
2023-01-19 03:10:17 +0100

    print movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
+const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-fetch(url)
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("Error: Could not retrieve movie information.");
-    }
-  })
-  .then(data => {
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
     console.log(data.title);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
+  } else {
+    console.log("Error: Could not retrieve movie information.");
+  }
+});
