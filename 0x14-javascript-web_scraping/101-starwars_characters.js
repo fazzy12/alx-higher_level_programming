e3f46ffc5622087a44aad3fa4cf045fb755b39a9
fazzy12 <kaluifeanyi00@gmail.com>
2023-01-25 23:18:34 +0100

    JQUERY: update jquery files

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require("request");
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const data = JSON.parse(body);
-    const charactersPromises = data.characters.map(characterUrl => {
+    const charactersPromises = data.characters.map((characterUrl) => {
       return new Promise((resolve, reject) => {
         request(characterUrl, (error, response, body) => {
           if (!error && response.statusCode === 200) {
@@ -19,15 +19,15 @@ request(url, (error, response, body) => {
       });
     });
     Promise.all(charactersPromises)
-      .then(characterNames => {
-        characterNames.forEach(name => {
+      .then((characterNames) => {
+        characterNames.forEach((name) => {
           console.log(name);
         });
       })
-      .catch(error => {
-        console.log('Error: Could not retrieve character information.');
+      .catch((error) => {
+        console.log("Error: Could not retrieve character information.");
       });
   } else {
-    console.log('Error: Could not retrieve movie information.');
+    console.log("Error: Could not retrieve movie information.");
   }
 });
