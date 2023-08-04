892743f6ab13f45773e40c869b55ce7e42abaf37
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-04 03:50:08 +0100

    Add Python script to print ASCII alphabet in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
-# ifeanyi kalu
-
 """Print the alphabet in lowercase, not followed by a new line."""
 for alpahbets in range(97, 123):
-    print(chr(alpahbets), end="")
+    print("{:c}".format(alpahbets), end="")
