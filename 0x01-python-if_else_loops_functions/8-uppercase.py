190faab2ad451e842ea30540602aeeb5abb39add
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-04 19:23:08 +0100

    Refactor the uppercase function for better readability and adherence to PEP8 guidelines.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     """Print a string in uppercase followed by a new line."""
-    for custom_str in str():
+    for custom_str in str:
         if ord(custom_str) >= 97 and ord(custom_str) <= 122:
             uppercase_char = chr(ord(custom_str) - 32)
         print("{}".format(uppercase_char), end="")
