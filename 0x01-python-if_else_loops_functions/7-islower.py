354667bb96234c85d1a36f36afe9a2f22df45a7a
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-04 18:19:55 +0100

    Add alternative implementations for the islower() function

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     """check if character is lowwer"""
-    if c >= 'a' and c <= 'z':
+    if ord(c) >= 'a' and ord(c) <= 'z':
         return True
     else:
         return False
