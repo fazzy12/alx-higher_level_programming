0dec03e8a5605caa5af878a599b5f17682c23d8b
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-05 13:53:09 +0100

    feat: Add script to print ASCII alphabet in reverse with alternating case

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for ascii_value in range(ord('Z'), ord('a') - 1, -1):
+for ascii_value in range(ord('z'), ord('Z') - 1, -1):
     character = chr(ascii_value)
-    if (ord('Z') - ascii_value) % 2 == 0:
-        character = character.lower()
+    if (ord('z') - ascii_value) % 2 == 0:
+        character = character.upper()
     print("{}".format(character), end="")
