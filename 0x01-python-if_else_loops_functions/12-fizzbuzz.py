3b2b974d449df5e0e4b516ecc83820a6eebf58e0
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-05 08:54:28 +0100

    print numbers from 1 to 100 seprated by s space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     """Print numbers from 1 to 100 separated by a space."""
-    for i in range(1, 101):
+    for i in range(1, 100):
         if i % 3 == 0 and i % 5 == 0:
             print("{}".format("FizzBuzz"), end=" ")
         elif i % 3 == 0:
