00b292dd69c93274908a6ae6d0a9e897343c4694
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-05 08:50:34 +0100

    print numbers from 1 to 100 seprated by s space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
         if i % 3 == 0 and i % 5 == 0:
             print("{}".format("FizzBuzz"), end=" ")
         elif i % 3 == 0:
-            print("{}".format("Fizz"), end="" )
+            print("{}".format("Fizz"), end=" ")
         elif i % 5 == 0:
             print("{}".format("Buzz"), end=" ")
         else:
