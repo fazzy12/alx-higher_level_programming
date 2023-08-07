be39655d39e0981c385931e5eda8723e8c31b8fd
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-07 23:13:30 +0100

    feat: Add program to import functions from calculator_1.py and handle basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,15 +2,16 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
+
 def main():
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
-    
+
     if operator == '+':
         result = add(a, b)
     elif operator == '-':
@@ -22,8 +23,9 @@ def main():
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    
+
     print("{} {} {} = {}".format(a, operator, b, result))
+
 
 if __name__ == "__main__":
     main()
