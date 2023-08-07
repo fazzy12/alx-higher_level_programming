167df7dc9eb4dca933c83cfc83dea3c60320200c
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-07 15:54:57 +0100

    feat: Add program for summing command-line integer arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-# 3-infinite_add.py
-# Ifeanyi kalu
+import sys
 
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    args = sys.argv[1:]  # Get command-line arguments excluding the script name
+    result = sum(int(arg) for arg in args)  # Sum all the integer arguments
+
+print("{}".format(result))
