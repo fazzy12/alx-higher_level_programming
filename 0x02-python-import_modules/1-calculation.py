ae756173ec6532ac7597acf39b15c435d9b90fbf
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-07 02:38:15 +0100

    imports functions from the file calculator_1.py, does some Maths, and prints the result.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-# 1-calculation.py
-# Ifeanyi kalu
 
 if __name__ == "__main__":
-    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    # import all the calc functions fom calculator_1.py
     from calculator_1 import add, sub, mul, div
 
+    # the value 10 to a variable a
     a = 10
+    # the value 5 to a variable b
     b = 5
 
+    # print the result of the addition of a and b
     print("{} + {} = {}".format(a, b, add(a, b)))
+    # print the result of the subtraction of a and b
     print("{} - {} = {}".format(a, b, sub(a, b)))
+    # print the result of the multiplication of a and b
     print("{} * {} = {}".format(a, b, mul(a, b)))
+    # print the result of the division of a and b
     print("{} / {} = {}".format(a, b, div(a, b)))
