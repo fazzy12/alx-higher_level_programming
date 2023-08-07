d7cb7fe0737154b712ace183bfb5fefe3e6244f4
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-07 04:37:12 +0100

    feat: Add program to print number of and list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,30 @@
+# #!/usr/bin/python3
+# import sys
+
+# if __name__ == "__main__":
+#     args = sys.argv[1:]
+#     num_args = len(args)
+
+#     if num_args == 0:
+#         print("0 arguments.")
+#     else:
+#         print("{} argument:".format(num_args))
+
+#         for i, arg in enumerate(args, start=1):
+#             print("{}: {}".format(i, arg))
+
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """Print the number of and list of arguments."""
     import sys
-    args = sys.argv[1:]
-    num_args = len(args)
 
-    if num_args == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} argument:".format(num_args))
-
-        for i, arg in enumerate(args, start=1):
-            print("{}: {}".format(i, arg))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
