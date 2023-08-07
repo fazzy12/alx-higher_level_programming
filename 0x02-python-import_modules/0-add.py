88c703e4eab2721fd13e57c63316198238683c3e
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-07 01:58:20 +0100

    Add function and main script for adding numbers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-# 0_add.py
-# Ifeanyi kalu
+from add_0 import add
 
-if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from add_0 import add
+def main():
+    """Main function to execute the addition.
 
+    This function imports the `add` function from the `add_0.py` module,
+    performs the addition of two numbers, and prints the result.
+    """
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+
+    # Call the add function to get the result
+    result = add(a, b)
+
+    # Display the result
+    print("{} + {} = {}".format(a, b, result))
+
+if __name__ == "__main__":
+    main()
