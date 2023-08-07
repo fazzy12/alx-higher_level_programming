04cf44e03d1994c8f741a9aab089ccf4bc943ab5
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-07 22:33:20 +0100

    feat: Add program to import and print variable `a` from `variable_load_5.py`

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# 5-variable_load.py
-# Ifeanyi kalu
-
 if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
     from variable_load_5 import a
 
-    print(a)
+    print("{}".format(a))
