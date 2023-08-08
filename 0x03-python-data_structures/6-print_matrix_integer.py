b9ef87399dee24a6141986dd7142fab0976395ca
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-08 17:40:00 +0100

    feat: Add solution for printing a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-# 6-print_matrix_integer.py
-# Ifeanyi kalu
-
-
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in row:
-            if i is not row[len(row) - 1]:
-                print("{:d}".format(i), end=" ")
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]), end="")
             else:
-                print("{:d}".format(i), end="")
+                print("{:d}".format(row[i]), end=" ")
         print()
