c783ced9c82a1d30fa2d7a725fbe80cf46d3d3a6
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-12 23:10:44 +0100

    Implemented square_matrix_simple function

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 def square_matrix_simple(matrix=[]):
-    squared_matrix = []
-    for row in matrix:
-        squared_row = []
-        for num in row:
-            squared_row.append(num**2)
-        squared_matrix.append(squared_row)
-    return squared_matrix
+    """ Computes the square value of all integers of a matrix """
+    return [[x**2 for x in row] for row in matrix]
