1e3120962e9308267e5f81b8806b5b6ed01f3afb
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-14 03:04:04 +0100

    Add function to calculate weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-# 100-weight_average.py
-# ifeanyi kalu
-
-
 def weight_average(my_list=[]):
-    """Return the weighted average of all integers in a list of tuples."""
-    if not isinstance(my_list, list) or len(my_list) == 0:
-        return (0)
+    if not my_list:
+        return 0
 
-    avg = 0
-    size = 0
-    for tup in my_list:
-        avg += (tup[0] * tup[1])
-        size += tup[1]
-    return (avg / size)
+    total_score = 0
+    total_weight = 0
+    
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+        
+    if total_weight == 0:
+        return 0
+    
+    return total_score / total_weight
