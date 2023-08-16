b73f4aa8f684ff1cd3627841beb1ada2e5ea628c
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-17 00:41:07 +0100

    feat: add prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,20 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
         print()
         return i + 1
     except IndexError:
         print()
-        return i
+        return i 
+
+# Test cases
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
