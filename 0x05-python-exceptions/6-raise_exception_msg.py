18a04d5c9bbe9c1719c407bdaec54e664757dcb3
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-18 16:48:08 +0100

    feat: raises a name exception with a message.

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-# 6-raise_exception_msg.py
-# ifeanyi kalu
-
-
 def raise_exception_msg(message=""):
-    """Raise a NameError exception with a message."""
     raise NameError(message)
