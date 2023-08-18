ad3113031b4b6d3d5da4f7a24a1205558a4895a4
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-18 03:55:44 +0100

    feat:  divides 2 integers and prints the result.

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-# 3-safe_print_division.py
-# ifeanyi kalu
-
-
 def safe_print_division(a, b):
-    """Returns the division of a by b."""
     try:
-        div = a / b
-    except (TypeError, ZeroDivisionError):
-        div = None
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+
     finally:
-        print("Inside result: {}".format(div))
-    return (div)
+        print("Inside result: {}".format(result))
+        return result
+
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
