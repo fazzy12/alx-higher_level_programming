ff475b42bb3910c13db47259c976ed1780dc28d7
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-18 18:18:52 +0100

    feat: add decifer a python bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -6,7 +6,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             result += (a ** b) / i
-        except:
-            result =  a + b
+        except Exception:
+            result = a + b
             break
-return result
+    return result
