85c44a47c4c35c89dbe22c21c87b80d3ed0449ce
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-24 01:18:09 +0100

    Add Square Class Definition with Constructor

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-# 2-square.py
-# ifeanyi kalu
-"""Define a class Square."""
+""" This module defines a Square class"""
 
 
 class Square:
-    """Represent a square."""
-
+    """Square class"""
     def __init__(self, size=0):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        if not isinstance(size, int):
+        """Initializes the data"""
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+            raise ValueError("ize must be >= 0")
+        else:
+            self.__size = size
