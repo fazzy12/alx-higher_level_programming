5d71e4f7420654ac13cebe01bf339139e0ff793c
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-24 01:28:50 +0100

    Add Square Class Definition with Constructor

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,6 +9,6 @@ class Square:
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("ize must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
