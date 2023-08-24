77821b0a9f468da419b8ba231419a58b994ad5a6
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-24 16:21:32 +0100

    Implement Square class with area and comparison methods

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -22,7 +22,7 @@ class Square:
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("size must be a number")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
