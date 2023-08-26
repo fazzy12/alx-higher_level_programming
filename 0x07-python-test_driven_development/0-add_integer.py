7a5ed91fb80d9ab861df5ea102b04b4bbd2633ac
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-26 15:12:13 +0100

    Implement the add function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,10 +16,9 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If either a or b is not an integer or a float.
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, int) and not isinstance(b, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    elif not isinstance(a, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return (a + b)
+    else:
+        return int(a) + int(b)
