465c8a8afb9e36d28a3fefe62f84d8342e112004
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-26 14:49:18 +0100

    Implement the add function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,46 +14,6 @@ def add_integer(a, b=98):
 
     Raises:
         TypeError: If either a or b is not an integer or a float.
-
-    Examples:
-        >>> add_integer(3, 5)
-        8
-
-        >>> add_integer(2.5, 3.5)
-        6
-
-        >>> add_integer(5, 3.5)
-        8
-
-        >>> add_integer(-5, -10)
-        -15
-
-        >>> add_integer(-2.5, -3.5)
-        -6
-
-        >>> add_integer(5, -10)
-        -5
-
-        >>> add_integer(-2.5, 3.5)
-        1
-
-        >>> add_integer(7)
-        105
-
-        >>> add_integer("5", 3)
-        Traceback (most recent call last):
-            ...
-        TypeError: a must be an integer
-
-        >>> add_integer(5, "3")
-        Traceback (most recent call last):
-            ...
-        TypeError: b must be an integer
-
-        >>> add_integer("5", "3")
-        Traceback (most recent call last):
-            ...
-        TypeError: a must be an integer
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
