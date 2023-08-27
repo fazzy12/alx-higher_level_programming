165a0be669977ba305f44478940b399d470dac34
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-27 03:28:19 +0100

    Add say_my_name function

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
-# 3-say_my_name.py
-# ifeanyi kalu
-"""Defines a name-printing function."""
+"""
+    This module provides a function to print a formatted message with names.
 
+    The module contains a single function `say_my_name` that takes a first name
+    and an optional last name as parameters and prints a message using string
+    formatting. The function ensures that both names are provided as strings and
+    handles formatting accordingly.
+"""
 
 def say_my_name(first_name, last_name=""):
-    """Print a name.
+    """
+    Print a formatted message with the provided first and last names.
+
+    This function takes the first name and an optional last name as arguments,
+    and prints a message in the format "My name is <first_name> <last_name>".
 
     Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
+        first_name (str): The first name to be included in the message.
+        last_name (str, optional): The last name to be included in the message.
+            Defaults to an empty string.
+
     Raises:
-        TypeError: If either of first_name or last_name are not strings.
+        TypeError: If either `first_name` or `last_name` is not a string.
+
     """
+
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {} {}".format(first_name, last_name))
