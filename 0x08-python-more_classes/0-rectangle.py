8a6feb1817f0cead648400f34265613d1a32c383
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-31 11:06:13 +0100

    Create an empty class Rectangle to serve as a blueprint for defining rectangles.

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,7 +2,28 @@
 """
 This module defines the Rectangle class, which represents
 a geometric rectangle.
+
+Classes:
+    Rectangle: A geometric shape with width and height properties.
+    
+
+Attributes:
+    width (int): The width of the rectangle.
+    height (int): The height of the rectangle.
+
+Methods:
+    __init__(width=0, height=0): Initializes a Rectangle instance with given
+    width and height.
+    width(): Getter method to retrieve the width of the rectangle.
+    width(value): Setter method to set the width of the rectangle.
+    height(): Getter method to retrieve the height of the rectangle.
+    height(value): Setter method to set the height of the rectangle.
+
+Raises:
+    TypeError: If width or height is not an integer.
+    ValueError: If width or height is less than 0.
 """
+
 
 
 class Rectangle:
