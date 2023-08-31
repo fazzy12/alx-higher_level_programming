75c7761701e140dc85899bba2471c43180268cb2
fazzy12 <kaluifeanyi00@gmail.com>
2023-08-31 03:01:36 +0100

    Create an empty class Rectangle to serve as a blueprint for defining rectangles.

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+This module contains the definition of the Rectangle class,
+which serves as a blueprint for creating rectangle objects.
+"""
+
 class Rectangle:
+    """Class:
+    Rectangle: A geometric shape with width and height properties.
+    """
     pass
