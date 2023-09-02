e5d22c9fec27f82b0bdb2e9db43a8a952c4fcd2c
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-02 15:44:32 +0100

    Implement LockedClass with slots to restrict attribute creation except for first_name

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""This module contains class LockedClass.
+"""
 class LockedClass:
     """LockedClass prevents the user from dynamically creating new instance
     attributes, except if the new instance attribute is called first_name."""
+
     __slots__ = ['first_name']
