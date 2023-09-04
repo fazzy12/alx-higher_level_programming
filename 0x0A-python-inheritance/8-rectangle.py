ea80898851aa42740e1858547603ff45510ac486
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-04 18:59:40 +0100

    docs: Add documentation for Rectangle class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,7 +2,7 @@
 """Module containing the `Rectangle` class inheriting from
 `BaseGeometry` class.
 """
-BaseGeometry = __import__ = ("7-base_geometry").BaseGeometry
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
