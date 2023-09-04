ed756b27196fa77f6b16e5ac98c6e1d48019cb49
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-04 02:33:34 +0100

    feat: class MyList that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""The list module contains the class list"""
+"""
+The list module contains the class MyList.
+"""
 
 
 class MyList(list):
@@ -17,18 +19,10 @@ class MyList(list):
     """
 
 
-
-    def __init__(self):
-        return super().__init__()
-
-
 def print_sorted(self):
     """
     Print the list elements in ascending sorted order.
 
-    Usage:
-        my_list = MyList([4, 2, 7, 1, 5])
-        my_list.print_sorted()  # Output: [1, 2, 4, 5, 7]
     """
     sorted_list = sorted(self)
     print(sorted_list)
