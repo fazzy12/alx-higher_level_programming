1ae0d626fefa2f36a2f94376b17221635b6e9e90
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-04 03:56:43 +0100

    fix: Corrected isinstance function call in is_same_class function

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -14,7 +14,7 @@ def is_same_class(obj, a_class):
     Returns:
         True if 'obj' is an instance of 'a_class', otherwise False.
     """
-    if isinstance(obj, a_class):
+    if isinstance(obj) == a_class:
         return True
     else:
         return False
