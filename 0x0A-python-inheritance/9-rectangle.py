ab7bfbf1113117830d19c02e42beda9273fe51b9
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-04 19:54:23 +0100

    feat: Implement Rectangle class with inheritance from BaseGeometry

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
-# 9-rectangle.py
-# ifeanyi kalu
-"""Defines a class Rectangle that inherits from BaseGeometry."""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Module containing the `Rectangle` class inheriting from
+`BaseGeometry` class.
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represent a rectangle using BaseGeometry."""
+    """A class representing a rectangle, inheriting from BaseGeometry.
+
+    Attributes:
+        __width (int): The width of the rectangle (private).
+        __height (int): The height of the rectangle (private).
+
+    Args:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+
+    Raises:
+        TypeError: If width or height is not a positive integer.
+
+    Note:
+        The Rectangle class inherits from BaseGeometry and enforces
+        positive integer validation for both width and height.
+    """
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle.
-
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-        """
-        super().integer_validator("width", width)
+        """Initialize a Rectangle instance with width and height."""
+        # Validate width and height as positive integers
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        # Set width and height as private attributes
         self.__width = width
-        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """Return the area of the rectangle."""
+        """Implement area of the rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
-        """Return the print() and str() representation of a Rectangle."""
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__width) + "/" + str(self.__height)
-        return string
+        """Return a string representation of the rectangle."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
