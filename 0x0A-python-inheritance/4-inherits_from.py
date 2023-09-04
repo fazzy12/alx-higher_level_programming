83ec654a312ab46185b774553bbc40848f75f630
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-04 15:29:37 +0100

    feat: Implement inherits_from function for checking class inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,6 +14,6 @@ def inherits_from(obj, a_class):
         True if 'obj' is an instance of a class that inherits from 'a_class',
         otherwise False.
     """
-    if issubclass(isinstance(obj), a_class) and isinstance(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
