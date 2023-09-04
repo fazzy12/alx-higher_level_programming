bf973f8dab15dc516eabb0d400b82362333f99e2
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-04 15:44:05 +0100

    docs: Add module-level and class-level documentation

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-# 5-base_geometry.py
-# ifeanyi kalu
-"""Defines an empty class BaseGeometry."""
+"""This module defines a base class for geometry-related operations.
+"""
 
 
 class BaseGeometry:
-    """Represent base geometry."""
+    """
+    An empty base class for geometry-related operations.
+
+    This class serves as a base for implementing geometry-related functionality
+    It currently doesn't have any attributes or methods but can be extended
+    by subclasses to provide specific geometric operations.
+
+    Attributes:
+        None.
+
+    Methods:
+        None.
+    """
     pass
