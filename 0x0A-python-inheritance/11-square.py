9abcd059bd786f6a7bbc343b1b88f6c0de98b71c
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-05 19:17:44 +0100

    feat: Implement Square class based on Rectangle

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -28,3 +28,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """Return a string representation of the square."""
+        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
