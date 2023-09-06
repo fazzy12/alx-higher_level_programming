2901da1a7a4315070ebc8d3b09b51f44a787f4ba
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-06 14:24:33 +0100

    Add to_json_string function for serializing objects to JSON

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-# 3-write_file.py
-# ifeanyi kalu
-"""Defines a file-writing function."""
+"""Module for to_json_string method that returns the JSON
+representation of an object (string)
+"""
 
 
-def write_file(filename="", text=""):
-    """Write a string to a UTF8 text file.
+import json
 
-    Args:
-        filename (str): The name of the file to write.
-        text (str): The text to write to the file.
-    Returns:
-        The number of characters written.
-    """
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+
+def to_json_string(my_obj):
+    return json.dumps(my_obj)
