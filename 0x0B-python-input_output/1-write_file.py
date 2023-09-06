91a93dd53a0cb0dfd70f50e4e069e092f591d5a2
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-06 12:58:19 +0100

    Add write_file function for writing text to files

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """Writes a string to a text file (UTF8) and returns the number of"""
-    with open(filename, encoding="utf=8") as file:
+    with open(filename, "w", encoding="utf=8") as file:
         file.write(text)
