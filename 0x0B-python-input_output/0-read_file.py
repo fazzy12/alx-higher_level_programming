6d71da21fc6aa737d8917ce8507a78002ef06e10
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-06 05:32:30 +0100

    Add read_file function for reading and printing text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python3
-# 0-read_file.py
-# ifeanyi kalu
-"""Defines a text file-reading function."""
+#!usr/bin/python3
+"""Module for read_file method that reads a file"""
 
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """Reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
