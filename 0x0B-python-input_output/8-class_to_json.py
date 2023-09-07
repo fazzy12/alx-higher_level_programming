8297da4d013cea14826d2534225fe0a5972925bc
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-07 05:16:50 +0100

    Add class_to_json function for custom object serialization

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,7 +4,7 @@ structure (list, dictionary, string, integer and boolean)"""
 
 
 def class_to_json(obj):
-    """Returns the dictionary description with simple data structure (list, 
+    """Returns the dictionary description with simple data structure (list,
     dictionary, string, integer and boolean) for JSON serialization
     of an object"""
 
