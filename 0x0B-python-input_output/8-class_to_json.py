89d6a0e2e2345adbe3eadc292b3387d770197b95
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-07 04:13:17 +0100

    Add class_to_json function for custom object serialization

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-# 8-load_from_json_file.py
-# ifeanyi kalu
-"""Defines a JSON file-reading function."""
-import json
+"""Defines a function that returns the dictionary description with simple data
+structure (list, dictionary, string, integer and boolean)"""
 
 
-def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
-    with open(filename) as f:
-        return json.load(f)
+def class_to_json(obj):
+    """Returns the dictionary description with simple data structure (list, 
+    dictionary, string, integer and boolean) for JSON serialization
+    of an object"""
+
+    if not hasattr(obj, "__dict__"):
+        return {}
+    return obj.__dict__
+
+    serialized = {}
+    for key, value in obj.__dict__.items():
+        if type(value) in [str, int, bool, list, dict]:
+            serialized[key] = value
+        else:
+            serialized[key] = value.__dict__
+    return serialized
