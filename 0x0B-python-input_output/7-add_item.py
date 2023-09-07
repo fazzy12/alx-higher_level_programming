fa13db61e1a0dce916605a43842793626f016f6a
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-07 03:51:58 +0100

    Refactor script to include local definitions of JSON-related functions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,10 +6,13 @@ import sys
 
 
 if __name__ == "__main__":
+    """Function that adds all arguments to a Python list, and then save them"""
+
     save_to_json_file = \
         __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
+
 
     try:
         items = load_from_json_file("add_item.json")
