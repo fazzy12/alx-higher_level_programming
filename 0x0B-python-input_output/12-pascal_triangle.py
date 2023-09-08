fcb4d83718e952a916d5a008572d47f5119a7eea
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-08 02:28:49 +0100

    returns a list of lists of integers representing the Pascal’s triangle of n

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python3
-# 12-student.py
-# ifeanyi kalu
-"""Defines a class Student."""
 
+def pascal_triangle(n):
+    if n <= 0:
+        return []
 
-class Student:
-    """Represent a student."""
+    triangle = []
 
-    def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+    for i in range(n):
+        row = []
 
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+        row.append(1)
 
-    def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
+        if i > 1:
+            for j in range(1, i):
+                value = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                row.append(value)
 
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
+        if i > 0:
+            row.append(1)
 
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        triangle.append(row)
+    return triangle
