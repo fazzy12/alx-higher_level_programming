a51c52ece43c041cffa6a0f0d4def601bd7e92fc
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-08 04:25:34 +0100

    feat: Add function for inserting text after specified line

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""This module contains a function that inserts a line of text to a file, 
+"""This module contains a function that inserts a line of text to a file,
 after the specified line number of text has been inserted intothe file"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Inserts a line of text to a file, after the specified line number of text
-    has been inserted intothe file and returns nothing (void) otherwise returns nothing (void) 
+    """Inserts a line of text to a file, after the specified line number
+    of text has been inserted intothe file and returns nothing (void)
+    otherwise returns nothing (void)
     """
-    
-    if not filename or not search_string or not new_string:
-        return
 
-    with open(filename, 'r+') as file:
-        lines = file.readlines()
-        file.seek(0)
-
+    with open(filename, 'r+') as f:
+        lines = f.readlines()
+        new_lines = []
         for line in lines:
-            file.write(line)
-
+            new_lines.append(line)
             if search_string in line:
-                file.write(new_string + '\n')
+                new_lines.append(new_string)
+        f.seek(0)
+        f.writelines(new_lines)
