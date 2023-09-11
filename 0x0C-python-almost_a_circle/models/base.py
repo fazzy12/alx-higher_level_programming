7a533f486238bd41feab35bf37a3742cc68d7d9f
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-11 01:54:34 +0100

    feat(models): Create Rectangle class with private attributes and getters/setters

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,3 +17,30 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+
+class Rectangle(Base):
+    """ Class Rectangle that inherits from Base class
+
+    Attributes:
+        width (int): width of the rectangle
+        height (int): height of the rectangle
+        x (int): x coordinate of the rectangle
+        y (int): y coordinate of the rectangle
+        id (int): id of the rectangle
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Constructor method for Rectangle class
+
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+            x (int): x coordinate of the rectangle
+            y (int): y coordinate of the rectangle
+            id (int): id of the rectangle
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
