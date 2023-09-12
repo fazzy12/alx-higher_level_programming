eab71019c17e929988010f9cd44e57a77ec40865
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-12 22:54:46 +0100

    feat(models): Create Base class for managing id attribute

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,35 +12,13 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
         if id is None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-
-class Rectangle(Base):
-    """ Class Rectangle that inherits from Base class
-
-    Attributes:
-        width (int): width of the rectangle
-        height (int): height of the rectangle
-        x (int): x coordinate of the rectangle
-        y (int): y coordinate of the rectangle
-        id (int): id of the rectangle
-    """
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """ Constructor method for Rectangle class
-
-        Args:
-            width (int): width of the rectangle
-            height (int): height of the rectangle
-            x (int): x coordinate of the rectangle
-            y (int): y coordinate of the rectangle
-            id (int): id of the rectangle
-        """
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
