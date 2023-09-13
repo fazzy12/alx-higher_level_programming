cb9e2593edfd2f3382705e43c9d46585ebfbaa31
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-13 20:03:16 +0100

    feat(models): Implement Rectangle class with attribute validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Rectangle class, inherits from Base."""
 
-from models.base import Base
 
+from models.base import Base
 
 class Rectangle(Base):
     """Rectangle class, inherits from Base."""
@@ -13,19 +13,19 @@ class Rectangle(Base):
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
-            x (int, optional): The x-coordinate of the rectangle's position.
-            Defaults to 0.
-            y (int, optional): The y-coordinate of the rectangle's position.
-            Defaults to 0.
+            x (int, optional): The x-coordinate of the rectangle's
+            position. Defaults to 0.
+            y (int, optional): The y-coordinate of the rectangle's
+            position. Defaults to 0.
             id (int, optional): The id to assign to the instance.
             Defaults to None.
         """
         super().__init__(id)  # Call the constructor of the base class (Base)
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width  # Use the setter method to validate width
+        self.height = height  # Use the setter method to validate height
+        self.x = x  # Use the setter method to validate x
+        self.y = y  # Use the setter method to validate y
 
     # Getter for width
     @property
@@ -35,8 +35,10 @@ class Rectangle(Base):
     # Setter for width
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("Width must be greater than 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     # Getter for height
@@ -47,8 +49,10 @@ class Rectangle(Base):
     # Setter for height
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("Height must be greater than 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     # Getter for x-coordinate
@@ -59,8 +63,10 @@ class Rectangle(Base):
     # Setter for x-coordinate
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("X-coordinate cannot be negative")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     # Getter for y-coordinate
@@ -71,6 +77,8 @@ class Rectangle(Base):
     # Setter for y-coordinate
     @y.setter
     def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("Y-coordinate cannot be negative")
+            raise ValueError("y must be >= 0")
         self.__y = value
