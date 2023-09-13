f32b7b528588d7e29ed5416dafb055c395590634
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-14 00:43:05 +0100

    feat(models): Enhance Square Class with size Getter and Setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,85 +1,42 @@
 #!/usr/bin/python3
-# square.py
-# ifeanyi kalu
-"""Defines a square class."""
+""" Square class, inherits from Rectangle. """
+
 from models.rectangle import Rectangle
 
-
 class Square(Rectangle):
-    """Represent a square."""
+    """Square class, inherits from Rectangle."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new Square.
+        """Initialize a Square instance.
 
         Args:
-            size (int): The size of the new Square.
-            x (int): The x coordinate of the new Square.
-            y (int): The y coordinate of the new Square.
-            id (int): The identity of the new Square.
+            size (int): The size of the square (both width and height).
+            x (int, optional): The x-coordinate of the square's position.
+            Defaults to 0.
+            y (int, optional): The y-coordinate of the square's position.
+            Defaults to 0.
+            id (int, optional): The id to assign to the instance.
+            Defaults to None.
         """
+
+        # Call the constructor of the base class (Rectangle)
         super().__init__(size, size, x, y, id)
 
+    # Override the __str__ method to return a custom string representation
+    def __str__(self):
+        """Return a custom string representation of the Square."""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    # Override the setter for size to ensure both width and height are updated
     @property
     def size(self):
-        """Get/set the size of the Square."""
         return self.width
 
     @size.setter
     def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value <= 0:
+            raise ValueError("size must be > 0")
         self.width = value
         self.height = value
-
-    def update(self, *args, **kwargs):
-        """Update the Square.
-
-        Args:
-            *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents size attribute
-                - 3rd argument represents x attribute
-                - 4th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
-        """
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
-                    self.size = arg
-                elif a == 2:
-                    self.x = arg
-                elif a == 3:
-                    self.y = arg
-                a += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
-
-    def to_dictionary(self):
-        """Return the dictionary representation of the Square."""
-        return {
-            "id": self.id,
-            "size": self.width,
-            "x": self.x,
-            "y": self.y
-        }
-
-    def __str__(self):
-        """Return the print() and str() representation of a Square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
-                                                 self.width)
