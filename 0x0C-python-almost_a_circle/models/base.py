efc194aa878e80391270043ee797075718fd8366
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-14 23:51:52 +0100

    feat(models): Add to_json_string Method to Base Class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Base class for all other classes in this project
 """
+import json
 
 
 class Base:
@@ -20,3 +21,20 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    # Static method to return the JSON string representation of list_dictionaries
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Convert a list of dictionaries to a JSON string.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries
+            to be converted to JSON.
+
+        Returns:
+            str: The JSON string representation of the input
+            list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
