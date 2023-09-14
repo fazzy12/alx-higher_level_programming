53303bc9ccc6f9bdfc768d6959676d5f0e78874b
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-15 00:31:55 +0100

    feat(models): Add Class Method class_method to MyClass

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,19 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of a list of instances file.
+
+        Args:
+            list_objs (list): A list of instances to be saved to a JSON file.
+        """
+        if list_objs is None:
+            list_objs = []
+
+        file_name = cls.__name__ + ".json"
+        with open(file_name, mode='w', encoding='utf-8') as file:
+            list_dicts = [obj.to_dictionary() for obj in list_objs]
+            json_string = cls.to_json_string(list_dicts)
+            file.write(json_string)
