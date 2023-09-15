037da104b9452f981d3a624d0855e85dbe611169
fazzy12 <kaluifeanyi00@gmail.com>
2023-09-15 13:43:54 +0100

    feat(models): Add CSV Serialization and Deserialization

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -118,15 +118,12 @@ class Base:
         Args:
             list_objs (list): A list of instances to be serialized and saved.
         """
-
-        objects = [obj.id, obj.width, obj.height, obj.x, obj.y]
-
         file_name = cls.__name__ + ".csv"
         with open(file_name, mode='w', newline='') as file:
             writer = csv.writer(file)
             if cls.__name__ == "Rectangle":
                 for obj in list_objs:
-                    writer.writerow(objects)
+                    writer.writerow([obj.id, obj.width, obj.height, obj.x, obj.y])
             elif cls.__name__ == "Square":
                 for obj in list_objs:
                     writer.writerow([obj.id, obj.size, obj.x, obj.y])
