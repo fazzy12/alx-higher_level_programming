a447b259c3fd4e1379a1ca26d6a5dd1735d3aa67
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-14 18:06:46 +0100

    creates a database if it does not already exist.

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- Creates the database hbtn_0c_0.
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+-- This SQL script creates a database if it does not already exist.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
