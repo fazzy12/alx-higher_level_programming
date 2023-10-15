d5563c0aad1c75094152693f565f0891f77f53cd
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-15 06:32:37 +0100

    creates a table called first_table in the current database

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,5 @@
--- Creates a table called first_table with values id and name.
-CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+-- Create a table first_table with two columns id and name
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
