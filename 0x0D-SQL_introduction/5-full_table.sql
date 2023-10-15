af8f1b68bccac0eec0b47eb94e498e0cf544dac4
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-15 09:09:24 +0100

    prints the full description of the table first_table from the database hbtn_0c_0

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
--- Prints the full description of the table first_table.
-SHOW CREATE TABLE `first_table`;
+-- Execute: mysql -uhbtn_0c_0 -p hbtn_0c_0 < 0x0D-SQL_introduction/5-full_table.sql
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+FROM information_schema.columns
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
