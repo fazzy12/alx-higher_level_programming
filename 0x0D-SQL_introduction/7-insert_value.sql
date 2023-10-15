4518ca6f0a7574b3ae146e3ce4a33d65071e1e72
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-15 09:57:10 +0100

    inserts a new row in the table first_table (database hbtn_0c_0)

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+-- Insert a new row in the table first_table with the following values:
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
