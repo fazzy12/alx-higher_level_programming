f99735a3e3968b27d3f75e8d3498be2c54502968
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-15 09:14:09 +0100

    prints the full description of the table first_table from the database hbtn_0c_0

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Execute: mysql -uhbtn_0c_0 -p hbtn_0c_0 < 0x0D-SQL_introduction/5-full_table.sql
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.columns
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW TABLE 'first_table';
