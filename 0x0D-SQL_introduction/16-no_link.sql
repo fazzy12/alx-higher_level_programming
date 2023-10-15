a222c8c915fd702eff65793a7704e8f35bfa04aa
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-15 19:43:09 +0100

    lists all records of the table second_table of the database hbtn_0c_0

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- Select records from the second_table with a name value,
 --   displaying score and name, ordered by descending score
 SELECT score, name
-FROM second_table 
+FROM second_table
 WHERE name IS NOT NULL
 ORDER BY score DESC;
