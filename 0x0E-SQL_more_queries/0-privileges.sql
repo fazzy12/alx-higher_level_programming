18bf93b3191abe4700a2c92f1ac68229677b239e
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-17 13:06:37 +0100

    Grant privileges to MySQL user 'user_0d_1' and list privileges in script

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2.
+-- This query lists all privileges of the MySQL user user_0d_1 & user_0d_2 on the localhost.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0_2'@'localhost';
