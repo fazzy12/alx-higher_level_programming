70f3aff630933c864c2019e6162c8e52fcab6862
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-17 14:42:22 +0100

    Add SQL query to create the table id_not_null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,7 @@
--- Creates the table id_not_null
-CREATE TABLE IF NOT EXISTS `id_not_null` (
-    `id`   INT          DEFAULT 1,
-    `name` VARCHAR(256)
+-- Create a table named id_not_null. This table should have:
+-- An id column that cannot be null and must have a default value of 1
+-- A name column that can be null
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256) NOT NULL,
 );
