01fd2592417d16dea417a32d67ddfa557d1fa0b7
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-17 14:32:01 +0100

    Create force_name table in the database with id and non-null name field

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Create force_name table in the database with id and non-null name field
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
-    name VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL
 );
