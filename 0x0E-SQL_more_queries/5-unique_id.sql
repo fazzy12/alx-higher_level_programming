a503c93979e4d2ec2baff2c8e699d6b5d3d6cd6c
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-18 01:07:49 +0100

    Create table unique_id with id as INT DEFAULT 1 UNIQUE and name as VARCHAR(256)

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,6 @@
--- Creates the table unique_id.
-CREATE TABLE IF NOT EXISTS `unique_id` (
-    `id`   INT          DEFAULT 1 UNIQUE,
-    `name` VARCHAR(256)
+-- a script that creates the table unique_id.
+-- id INT with the default value 1 and must be unique.
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
 );
