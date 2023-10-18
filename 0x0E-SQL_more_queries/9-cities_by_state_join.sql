9f1fbb01ae6515180b5a4f5add7323eaa6adccfd
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-18 03:57:02 +0100

    Add SQL query to list all cities in the hbtn_0d_usa database with their IDs, names, and corresponding states.

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,5 @@
--- Lists all cities in the database hbtn_0d_usa.
--- Records are sorted in order of ascending cities.id.
-SELECT c.`id`, c.`name`, s.`name`
-  FROM `cities` AS c
-       INNER JOIN `states` AS s
-       ON c.`state_id` = s.`id`
- ORDER BY c.`id`;
+-- script that lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id, cities.name, states.name
+FROM hbtn_0d_usa.cities, hbtn_0d_usa.states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
