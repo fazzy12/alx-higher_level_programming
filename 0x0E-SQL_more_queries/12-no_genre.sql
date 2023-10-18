d1bec435c72bcfd8f81b0b5a9f5b46fb5dbc4dc0
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-18 17:53:23 +0100

    Add SQL query to list shows without a genre in hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,7 @@
 -- This query uses a LEFT JOIN to retrieve information about
 -- TV shows and their associated genre IDs. 
-SELECT tv_shows.title, tv_show_genres.genre_id
-    FROM tv_shows
-        LEFT JOIN tv_show_genres 
-        ON tv_shows.id = tv_show_genres.tv_show_id
-            WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT shows.title, genres.genre_id
+FROM tv_shows AS shows
+LEFT JOIN tv_show_genres AS genres ON shows.id = genres.show_id
+WHERE genres.genre_id IS NULL
+ORDER BY shows.title, genres.genre_id;
