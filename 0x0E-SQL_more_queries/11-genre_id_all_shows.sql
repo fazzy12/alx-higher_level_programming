6b289d17a6c853b7c2c6f976e30741639b1db949
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-18 17:19:52 +0100

    Add SQL query for retrieving TV show information with associated genres

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,17 @@
--- Lists all shows contained in the database hbtn_0d_tvshows.
--- Displays NULL for shows without genres.
--- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
-SELECT s.`title`, g.`genre_id`
-  FROM `tv_shows` AS s
-       LEFT JOIN `tv_show_genres` AS g
-       ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`;
+/*
+   This SQL query retrieves information about TV shows and their associated genres.
+   It uses a LEFT JOIN to include all shows from the 'tv_shows' table, even if they
+   don't have a corresponding entry in the 'tv_show_genres' table. If a show has no
+   genre, the 'tv_show_id' in the 'tv_show_genres' table will be NULL for that record.
+
+   Result Columns:
+   - tv_shows.id: The unique identifier for each TV show.
+   - tv_shows.title: The title of the TV show.
+   - tv_show_genres.tv_show_id: The identifier linking a TV show to its genre(s).
+
+   Note: Rows with NULL 'tv_show_id' indicate shows with no associated genres.
+*/
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
