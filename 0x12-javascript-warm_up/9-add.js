6c4af8f52ac16dfa72de8582cfbede3587eba70e
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-30 14:53:36 +0100

    feat: Create JavaScript script to add two integers using a function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-function add (a, b) {
-  return a + b;
-}
+const args = process.argv.slice(2);
 
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+function add (a, b) {
+  a = args[0] = parseInt(args[0]);
+  b = args[1] = parseInt(args[1]);
+
+  return console.log(a + b);
+}
+add();
