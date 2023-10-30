fb3ac3e13f3f9f8f138ee3a3ee2272754a322dd2
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-30 05:28:25 +0100

    feat: Create JavaScript script to print lines from an array using a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const myArry = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myArry.length; i++) {
+  console.log(myArry[i]);
 }
