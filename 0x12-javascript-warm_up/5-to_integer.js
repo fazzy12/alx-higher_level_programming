bdeeac74a21a65a02e9f3ec2afb43e971bcd29b6
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-30 03:35:50 +0100

    feat: Create JavaScript script to handle integer conversion

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+const args = process.argv.slice(2);
+
+if (isNaN(args[0])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + args[0]);
+}
