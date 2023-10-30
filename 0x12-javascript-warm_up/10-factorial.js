1aad0feefc330aac37bec92ae1e9ca9a73ddf6b7
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-30 15:25:16 +0100

    feat: Create JavaScript script to compute and print the factorial of an integer

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -6,6 +6,6 @@ function factorial(n) {
   if (n === 0 || n === 1) {
     return 1;
   }
-    return n * factorial(n - 1);
-  }
+  return n * factorial(n - 1);
+}
 console.log(factorial(parseInt(process.argv[2])));
