f8ce2f238302531fba2b774cb7d6d0a656b71e56
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-01 16:25:53 +0100

    Added Rectangle class with constructor, print, rotate, and double methods

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,18 +1,33 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+// Rectangle that defines a rectangle
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
+  // Method that prints the rectangle using the character X
   print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+    const x = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(x.repeat(this.width));
+    }
   }
 
+  // Method that exchanges the width and the height of the rectangle
   rotate () {
-    [this.width, this.height] = [this.height, this.width];
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
   }
 
+  // Method that multiples the width and the height of the rectangle by 2
   double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+    this.width *= 2;
+    this.height *= 2;
   }
-};
+}
+
+module.exports = Rectangle;
