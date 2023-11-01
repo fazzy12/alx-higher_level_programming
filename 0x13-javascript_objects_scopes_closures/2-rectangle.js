9fa641ab545553a31bdfeb79964ab33197294fac
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-01 15:15:10 +0100

    Define the Rectangle class with validation

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,9 @@
 // Rectangle that defines a rectangle
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      this.width = {};
-      this.height = {};
+    if (w <= 0 && h <= 0) {
+      this.width = undefined;
+      this.height = undefined;
     } else {
       this.width = w;
       this.height = h;
