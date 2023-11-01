a80cf29545014e7f9a2099082ec3b07a0cd6f9d7
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-01 16:58:47 +0100

    Added Square class that inherits from Square and implements charPrint method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square.js') {
+// Square that defines a square and inherits from Square of 5-square.js.
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
   charPrint (c) {
     if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
-};
+}
+
+module.exports = Square;
