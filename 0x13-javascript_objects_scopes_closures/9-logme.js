e18230aab03f4d4265ae1dae350f9d1a07064997
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-01 18:23:01 +0100

    Add solution for task 9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
-let count = 0;
-exports.logMe = function (item) { console.log(`${count++}: ${item}`); };
+
+exports.logMe = function (item) {
+  if (this.count === undefined) {
+    this.count = 0;
+  }
+  console.log(this.count + ': ' + item);
+  this.count++;
+};
