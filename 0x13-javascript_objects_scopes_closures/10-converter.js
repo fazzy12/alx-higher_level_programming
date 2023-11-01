8548509a59361c7781c2e12d5650bbb11fc702fe
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-01 19:06:25 +0100

    Add solution for converting numbers from base 10 to another base using higher-order functions.

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-exports.converter = function (base) { return num => num.toString(base); };
+// Convert a number from base 10 to another base passed as argument.
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
