7bd9afbcf7fedcfb82d1a04f32d00131dd4489a1
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-01 17:03:20 +0100

    Added Square class that inherits from Rectangle and implements print and double methods

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./4-rectangle.js') {
-  constructor (size) {
+// Square that defines a square and inherits from Rectangle.
+const Rectangle = require("./4-rectangle");
+
+class Square extends Rectangle {
+  constructor(size) {
     super(size, size);
   }
-};
+}
+
+module.exports = Square;
