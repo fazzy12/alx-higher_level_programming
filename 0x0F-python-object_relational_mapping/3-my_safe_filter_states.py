12c07056f938d3785fc8b2980d218977287616e1
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-04 19:47:26 +0100

    Add 3-my_safe_filter_states.py script

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-"""
-This script takes in an argument and
-displays all values in the states
-where `name` matches the argument
-from the database `hbtn_0e_0_usa`.
-This time the script is safe from
-MySQL injections!
-"""
+"""Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument."""
 
-import MySQLdb as db
-from sys import argv
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    db_cursor = db_connect.cursor()
-    db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE \
-                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-    rows_selected = db_cursor.fetchall()
+    cursor = db.cursor()
 
-    for row in rows_selected:
+    # Construct the SQL query with placeholders and execute it safely
+    query = "SELECT * FROM states WHERE name=%s ORDER BY id"
+    cursor.execute(query, (state_name,))
+
+    # Fetch and display the results
+    results = cursor.fetchall()
+    for row in results:
         print(row)
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
