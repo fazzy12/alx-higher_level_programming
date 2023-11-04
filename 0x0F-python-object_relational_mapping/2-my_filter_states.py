47ea79dcc482a3c0943edb460469b132adfe711d
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-04 18:39:59 +0100

    Add 2-my_filter_states.py script

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python3
-"""
-This script takes in an argument and
-displays all values in the states
-where `name` matches the argument
-from the database `hbtn_0e_0_usa`.
-"""
+import sys
+import MySQLdb
 
-import MySQLdb as db
-from sys import argv
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
-    db_cursor = db_connect.cursor()
+    # Connect to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-    db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-                        states.id ASC".format(argv[4]))
-    rows_selected = db_cursor.fetchall()
+    # Create a cursor object
+    cursor = db.cursor()
 
-    for row in rows_selected:
+    # Execute the query
+    query = "SELECT * FROM states WHERE name LIKE BINARY \
+        '{}' ORDER BY id ASC".format(state_name)
+
+    cursor.execute(query)
+
+    # Fetch and display the results
+    results = cursor.fetchall()
+    for row in results:
         print(row)
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
