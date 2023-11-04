0ec2fb3af617813499bfff17933c1600268a1fc2
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-04 20:27:20 +0100

    Add 5-filter_cities.py script

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
-"""
-This script  takes in the name of a state
-as an argument and lists all cities of that
-state, using the database `hbtn_0e_4_usa`.
-"""
+"""script that takes the name of a state as an argument
+and lists all cities of that state from the hbtn_0e_4_usa"""
 
-import MySQLdb as db
-from sys import argv
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the cities
-    from the database.
-    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-    with db_connect.cursor() as db_cursor:
-        db_cursor.execute("""
-            SELECT
-                cities.id, cities.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            WHERE
-                states.name LIKE BINARY %(state_name)s
-            ORDER BY
-                cities.id ASC
-        """, {
-            'state_name': argv[4]
-        })
-        rows_selected = db_cursor.fetchall()
+    cursor = db.cursor()
 
-    if rows_selected is not None:
-        print(", ".join([row[1] for row in rows_selected]))
+    # Construct the SQL query with placeholders and execute it safely
+    query = "SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') FROM cities JOIN\
+        states ON cities.state_id = states.id\
+            WHERE states.name=%s ORDER BY cities.id"
+    cursor.execute(query, (state_name,))
+
+    # Fetch and display the results
+    result = cursor.fetchone()
+    if result:
+        print(result[0])
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
