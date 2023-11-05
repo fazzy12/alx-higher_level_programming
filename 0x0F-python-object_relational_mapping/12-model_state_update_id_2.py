57c91b21394c1965d7c0e590a9845717501b1d85
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-05 13:11:46 +0100

    Update State object with id 2 to 'New Mexico'

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 """
-This script changes the name of a State object
-from the database `hbtn_0e_6_usa`.
+This script changes the name of a State object with id 2 in the database hbtn_0e_6_usa.
 """
 
-from sys import argv
-from model_state import State, Base
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    Updates a State object on the database.
-    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
 
-    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
-    state = session.query(State).filter(State.id == 2).first()
-    state.name = "New Mexico"
-    session.commit()
+    state_to_update = session.query(State).filter_by(id=2).first()
+
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
 
     session.close()
