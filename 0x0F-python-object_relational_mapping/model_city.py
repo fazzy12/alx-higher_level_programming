34dfb176a51e0400c21d0847384bb9107e80d962
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-05 14:46:20 +0100

    Add model_city.py and 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base, State
 
+
 class City(Base):
     """City class to represent the cities table"""
     __tablename__ = 'cities'
