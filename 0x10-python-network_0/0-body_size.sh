a7c339c037607d779e5e759bd4dacc63f43ea4b7
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-21 16:33:56 +0100

    docs: Add Bash script for retrieving HTTP response size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+
+# Script to send a request to a URL and display the size of the response body
+
+# Check if URL argument is provided
+if [ -z "$1" ];
+then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send request and display size of the response body in bytes
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r'
