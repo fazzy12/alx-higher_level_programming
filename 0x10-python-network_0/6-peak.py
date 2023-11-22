74d1b4779c4c38575947253de290921527adf389
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-22 14:40:35 +0100

    feat: Implement peak-finding algorithm using binary search

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""Defines a peak-finding algorithm."""
-
+""" Find a peak in a list of unsorted integers. """
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    """
+    Find a peak in a list of unsorted integers.
+
+    Args:
+    - list_of_integers: A list of unsorted integers.
+
+    Returns:
+    - A peak element from the list.
+    """
+    if not list_of_integers:
         return None
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    low, high = 0, len(list_of_integers) - 1
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
