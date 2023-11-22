c8bf202b51226b0a8dbab8407569ff3eb8d95d48
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-22 23:04:51 +0100

    feat: Implement peak-finding algorithm using binary search

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,17 +12,23 @@ def find_peak(list_of_integers):
     Returns:
     - A peak element from the list.
     """
-    if not list_of_integers:
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
         return None
 
-    low, high = 0, len(list_of_integers) - 1
-
-    while low < high:
-        mid = (low + high) // 2
-
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            high = mid
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
         else:
-            low = mid + 1
-
-    return list_of_integers[low]
+            return list_of_integers[mid]
