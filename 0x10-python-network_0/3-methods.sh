c08a2735bf1d05825ff9d46e2f72838df5b1b64b
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-22 05:15:26 +0100

    feat: Add Bash script for displaying allowed HTTP methods with curl

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send an OPTIONS request using curl and display allowed HTTP methods
-curl -sI -X OPTIONS "$1" | grep -i Allow | awk '{print $2}'
+curl -sI "$1" | grep "Allow" | awk '{print $2}'
