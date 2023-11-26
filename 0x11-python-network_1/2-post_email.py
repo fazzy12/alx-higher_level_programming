68fa22d1f05304671dc3f28444512baf53a46157
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-26 18:06:42 +0100

    Add Python script for sending POST request with email parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
 """
-import sys
-import urllib.parse
-import urllib.request
+Send a POST request with an email parameter
+"""
 
+import sys
+import urllib.request
+import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    email = sys.argv[2]
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read().decode('utf-8')
+        print(body)
