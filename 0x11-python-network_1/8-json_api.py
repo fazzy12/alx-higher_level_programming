77a40b50e4a27ccdcebe1a4bdc7ebb97e3a89b19
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-26 20:17:39 +0100

    8. Search API: Python script to send a POST request to http://0.0.0.0:5000/search_user with a letter as a parameter.

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
-
-Usage: ./8-json_api.py <letter>
-  - The letter is sent as the value of the variable `q`.
-  - If no letter is provided, sends `q=""`.
 """
-import sys
-import requests
+8. Search API
+"""
 
+import requests
+import sys
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = r.json()
-        if response == {}:
-            print("No result")
+        response = requests.post(
+            'http://0.0.0.0:5000/search_user', data={'q': q})
+        json_data = response.json()
+
+        if json_data:
+            print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("No result")
     except ValueError:
         print("Not a valid JSON")
