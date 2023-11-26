958a8a0951e50d77247d0bf8651ddbfe0c15f564
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-26 17:04:41 +0100

    Add Python script for fetching X-Request-Id header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
-
-Usage: ./1-hbtn_header.py <URL>
 """
-import sys
-import urllib.request
+Takes in a URL, sends a request to the URL, and displays the value of the
+X-Request-Id variable found in the header of the response.
+"""
 
+import urllib.request
+import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    with urllib.request.urlopen(url) as response:
+        # Check if the 'X-Request-Id' header is present
+        if 'X-Request-Id' in response.headers:
+            x_request_id = response.headers['X-Request-Id']
+            print(x_request_id)
