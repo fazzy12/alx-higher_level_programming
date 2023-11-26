adec0a28fbc3cdabac87d75b2380c7ba494de0a6
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-26 18:39:16 +0100

    Add script to fetch status using requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""Script to fetch the status of https://alx-intranet.hbtn.io using requests"""
+
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
