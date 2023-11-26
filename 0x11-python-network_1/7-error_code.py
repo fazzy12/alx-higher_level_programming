dad7811f8b3c0fd9ce5b2cf2d3338f4fea67c102
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-26 20:00:47 +0100

    7. Error code #1: Python script to make a GET request to a URL and display the response body.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
-
-Usage: ./7-error_code.py <URL>
-  - Handles HTTP errors.
 """
-import sys
-import requests
+Python script that takes in a URL, sends a request to the URL, and
+displays the body of the response. If the HTTP status code is greater
+than or equal to 400, prints: "Error code:" followed by the value of
+the HTTP status code.
+"""
 
+import requests
+import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
-        print(r.text)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+        print(response.text)
+    except requests.exceptions.HTTPError as errh:
+        print(f"Error code: {errh.response.status_code}")
+    except Exception as err:
+        print(err)
