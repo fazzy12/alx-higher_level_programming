9acb84f495f79cc1c13baaa99844c584238f3347
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-26 19:14:16 +0100

    6. POST an email #1 - Python script that sends a POST request to a given URL with an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-
-Usage: ./6-post_email.py <URL> <email>
-  - Displays the body of the response.
 """
-import sys
-import requests
+6. POST an email #1
+"""
 
+import requests
+import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    email = sys.argv[2]
 
-    r = requests.post(url, data=value)
-    print(r.text)
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    print("Your email is: {}".format(response.text))
