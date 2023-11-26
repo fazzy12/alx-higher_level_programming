02492edd08f61dc4d081baa1720a681a21382f44
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-26 18:26:20 +0100

    Add script to handle HTTPError exceptions and display response body

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
-
-Usage: ./3-error_code.py <URL>
-  - Handles HTTP errors.
 """
-import sys
-import urllib.error
-import urllib.request
+Send a request to a URL and display the body of the response
+Handle urllib.error.HTTPError exceptions and print the HTTP status code
+"""
 
+import sys
+import urllib.request
+import urllib.error
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+
+            print(body)
     except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        print("Error code:", e.code)
