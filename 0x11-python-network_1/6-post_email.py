fa0bb2493d929c505ab874de44cbf674ef880564
fazzy12 <kaluifeanyi00@gmail.com>
2023-11-26 19:53:36 +0100

    6. POST an email #1 - Python script that sends a POST request to a given URL with an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
 
     payload = {'email': email}
     response = requests.post(url, data=payload)
-    print("Your email is: {}".format(response.text))
+    print(response.text)
