98760b91337abb597964620903596b00ac8e0583
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-22 17:58:18 +0100

    Add script for reading and printing file content

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filepath = process.argv[2];
 
-fs.readFile(filepath, 'utf-8', (err, data) => {
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+    console.error(err);
+    return;
   }
+  console.log(data);
 });
