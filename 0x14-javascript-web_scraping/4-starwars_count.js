1c8a27bb7c68c94f0ee3477e9393bdc9e012c997
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-22 22:14:06 +0100

    Add script to count movies with character "Wedge Antilles"

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const results = JSON.parse(body).results;
-    console.log(results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0));
+
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
   }
+
+  const data = JSON.parse(body);
+  let count = 0;
+  for (const film of data.results) {
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
 });
