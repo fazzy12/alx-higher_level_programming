6af91a27658a9eb36fe29084fde63290bd762548
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-23 08:09:42 +0100

    Implement script to fetch webpage content and store in file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const request = require("request");
-const fs = require("fs");
+const request = require('request');
+const fs = require('fs');
 
 request(process.argv[2], function (err, response, body) {
   if (err == null) {
