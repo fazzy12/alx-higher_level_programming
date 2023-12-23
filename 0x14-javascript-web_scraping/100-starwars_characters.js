0bd559185662c40aab8681d6a94a4a7ea03631f8
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-23 08:57:58 +0100

    Add script to retrieve and display characters of a given Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,22 +1,38 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const data = JSON.parse(body);
-    data.characters.forEach(character => {
-      request(character, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        } else {
-          console.log('Error: Could not retrieve character information.');
-        }
-      });
-    });
-  } else {
-    console.log('Error: Could not retrieve movie information.');
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
   }
+
+  if (response.statusCode !== 200) {
+    console.error(`Error: ${response.statusCode}`);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const charactersUrls = movie.characters;
+
+  charactersUrls.forEach(characterUrl => {
+    request.get(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.error(`Error: ${response.statusCode}`);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
 });
