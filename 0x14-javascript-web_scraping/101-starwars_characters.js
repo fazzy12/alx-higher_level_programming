1988972f85107adfb8878eca63f130f0be9d4f0f
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-23 09:14:35 +0100

    Refactor to ensure correct order of Star Wars characters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,33 +1,36 @@
 #!/usr/bin/node
-const request = require("request");
+
+const request = require('request');
+
 const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const data = JSON.parse(body);
-    const charactersPromises = data.characters.map((characterUrl) => {
-      return new Promise((resolve, reject) => {
-        request(characterUrl, (error, response, body) => {
-          if (!error && response.statusCode === 200) {
-            const characterData = JSON.parse(body);
-            resolve(characterData.name);
-          } else {
-            reject(error);
-          }
-        });
-      });
-    });
-    Promise.all(charactersPromises)
-      .then((characterNames) => {
-        characterNames.forEach((name) => {
-          console.log(name);
-        });
-      })
-      .catch((error) => {
-        console.log("Error: Could not retrieve character information.");
-      });
-  } else {
-    console.log("Error: Could not retrieve movie information.");
+  if (error) {
+    console.error(error);
+    return;
   }
+
+  const film = JSON.parse(body);
+  const characterUrls = film.characters;
+
+  // Using a recursive function to handle asynchronous requests in sequence
+  function fetchCharacter (index) {
+    if (index >= characterUrls.length) return;
+
+    request(characterUrls[index], (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+
+      // Fetch the next character
+      fetchCharacter(index + 1);
+    });
+  }
+
+  fetchCharacter(0);
 });
