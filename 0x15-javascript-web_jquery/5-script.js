dcec7012b86b5ce1d011c0418ee496760737af9c
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-29 11:51:15 +0100

    Add script to append <li> element to list on user click

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
-  const $addItemButton = $("#add_item");
-  const $list = $("ul.my_list");
-
-  $addItemButton.on("click", function () {
-    const $newListItem = $("<li>Item</li>");
-    $list.append($newListItem);
+  $('#add_item').click(function () {
+    $('.my_list').append('<li>Item</li>');
   });
 });
