b08ce9b6a50b33d58fd83576031f4fc32563e596
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-29 12:52:41 +0100

    Add functionality to fetch and display translations of "Hello" on button click or ENTER key press

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,17 +1,20 @@
-$('document').ready(function () {
-  $('INPUT#btn_translate').click(translate);
-  $('INPUT#language_code').focus(function () {
-    $(this).keydown(function (e) {
-      if (e.keyCode === 13) {
-        translate();
-      }
+$(document).ready(function () {
+  function fetchTranslation () {
+    const langCode = $('#language_code').val();
+
+    // Fetch the translation from the API
+    $.getJSON(`https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`, function (data) {
+      $('#hello').text(data.hello);
     });
+  }
+
+  // Fetch translation when the button is clicked
+  $('#btn_translate').click(fetchTranslation);
+
+  // Fetch translation when ENTER is pressed on the language code input
+  $('#language_code').keypress(function (event) {
+    if (event.which === 13) { // 13 is the ENTER key code
+      fetchTranslation();
+    }
   });
 });
-
-function translate () {
-  const url = 'https://www.fourtonfish.com/hellosalut/?';
-  $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
-    $('DIV#hello').html(data.hello);
-  });
-}
