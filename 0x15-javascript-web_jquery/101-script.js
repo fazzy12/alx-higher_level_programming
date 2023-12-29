cf73f15222fb6ee4dad49ba2dfe18abfa2121c26
fazzy12 <kaluifeanyi00@gmail.com>
2023-12-29 12:39:22 +0100

    Add script to handle adding, removing, and clearing list items

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-  // Add new element to list
-  $("#add_item").click(function () {
-    $("UL.my_list").append("<li>Item</li>");
+  // Add item to the list
+  $('#add_item').click(function () {
+    $('ul.my_list').append('<li>Item</li>');
   });
 
-  // Remove last element from list
-  $("#remove_item").click(function () {
-    $("UL.my_list li:last-child").remove();
+  // Remove last item from the list
+  $('#remove_item').click(function () {
+    $('ul.my_list li:last-child').remove();
   });
 
-  // Clear all elements from list
-  $("#clear_list").click(function () {
-    $("UL.my_list").empty();
+  // Clear the entire list
+  $('#clear_list').click(function () {
+    $('ul.my_list').empty();
   });
 });
